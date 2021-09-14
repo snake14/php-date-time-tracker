05f6caf50792f8d4ca6f1a91a0b35eb1ed4bfdf9
Jacob Ransom <snakeransom@gmail.com>
2021-09-14 14:26:10 -0700

    Switching to view templating.

--- src/config/sample_config.php
+++ src/config/sample_config.php
@@ -4,8 +4,6 @@
 		'servername' => 'hostname',
 		'username' => 'username',
 		'password' => 'some_password',
-		'dbName' => 'med_tracker',
-		'google_client_id' => 'your_google_api_client_id_for_this_project',
-		'allowed_user_emails' => [ 'some.email@domain.com', 'another.email@domain.com' ] // This should eventually be moved to the database.
+		'dbName' => 'some_database_name'
 	];
 ?>
--- src/controllers/Main.php
+++ src/controllers/Main.php
@@ -2,50 +2,42 @@
 namespace Controllers;
 
 class Main {
-	public function getLoginPage($f3, $params) {
-		$isLocalNetwork = ($_SERVER['SERVER_NAME'] === '192.168.1.100') ? 'true' : 'false';
-
-		if($isLocalNetwork === 'true') {
-			if(!isset($_SERVER['PHP_AUTH_USER'])) {
-				header('WWW-Authenticate: Basic realm="My Realm"');
-				header('HTTP/1.0 401 Unauthorized');
-				echo 'Login Required';
+	public function getMainPage($f3, $params) {
+		// Check to make sure that the user has logged in.
+		if(!isset($_SERVER['PHP_AUTH_USER'])) {
+			header('WWW-Authenticate: Basic realm="My Realm"');
+			header('HTTP/1.0 401 Unauthorized');
+			echo 'Login Required';
+			exit;
+		} else {
+			$DB = $f3->get('db');
+			if(empty($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] !== 'snake14' || empty($_SERVER['PHP_AUTH_PW']) || !$DB->isLocalPassValid($_SERVER['PHP_AUTH_PW'])) {
+				echo 'Login Failed';
 				exit;
-			} else {
-				$db = $f3->get('db');
-				if(empty($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] !== 'snake14' || empty($_SERVER['PHP_AUTH_PW']) || !$db->isLocalPassValid($_SERVER['PHP_AUTH_PW'])) {
-					echo 'Login Failed';
-					exit;
-				}
 			}
 		}
-		// Load the login page.
-		include __DIR__ . '/../../views/login.php';
-	}
 
-	public function getMainPage($f3, $params) {
-		// Load the main page.
+		// Load the main page data.
 		$DB = $f3->get('db');
 		$result = $DB->getAllRecords();
 		$DB->disconnect();
-		
-		$most_recent_date_string = '';
-		$most_recent_time_string = '';
 
 		$most_recent_time = $result['records'][0]['dt'];
 		$date1 = new \DateTime();
 		$date2 = new \DateTime($most_recent_time);
-		$most_recent_date_string = $date2->format('m/d/y');
-		$most_recent_time_string = $date2->format('h:i a');
 		$diff = $date1->diff($date2);
 		$diff_days = $diff->format('%d days');
 		$diff_result = ($diff_days != '0 days' ? $diff_days.', ' : '').$diff->format('%H hours').', '.
 			$diff->format('%i minutes').' since the last record';
 
-		$isLocalNetwork = ($_SERVER['SERVER_NAME'] === '192.168.1.100') ? 'true' : 'false';
-		$hasHttpAuth = (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) ? 'true' : 'false';
+		$records = [];
+		if(!empty($result['success']) && !empty($result['records']) && is_array($result['records'])) {
+			$records = $result['records'];
+		}
+		$f3->set('records', $records);
+		$f3->set('mostRecentRecord', $diff_result);
 
-		include __DIR__ . '/../../views/main.php';
+		echo \Template::instance()->render('main.htm');
 	}
 
 	public function getEditPage($f3, $params) {
@@ -54,8 +46,11 @@ class Main {
 		$id = $params['id'];
 		$result = $DB->getRecordById($id);
 		$DB->disconnect();
-		$date_time = date('m/d/y h:i a', strtotime($result['records']['dt']));
-		include __DIR__ . '/../../views/edit.php';
+		$dateTime = date('m/d/y h:i a', strtotime($result['records']['dt']));
+		$f3->set('id', $id);
+		$f3->set('dateTime', $dateTime);
+
+		echo \Template::instance()->render('edit.htm');
 	}
 
 	public function update($f3, $params) {
